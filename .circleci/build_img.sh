192a178a289644f3266720bbf2cf59afe899ef98
alamscode <ahmed@eurustechnologies.com>
2020-02-27 03:51:47 +0500

    changes

--- .circleci/build_img.sh
+++ .circleci/build_img.sh
@@ -1,5 +1,5 @@
 set -x
-aws ecr get-login-password | docker login --username AWS --password-stdin 020046395185.dkr.ecr.us-east-1.amazonaws.com/aws-cli
+$(aws ecr get-login --no-include-email --region us-east-1)
 docker build -t aws-cli ../Docker/SimpleApp/
 docker tag aws-cli:latest 020046395185.dkr.ecr.us-east-1.amazonaws.com/aws-cli:latest
 # docker push 020046395185.dkr.ecr.us-east-1.amazonaws.com/aws-cli:latest
