3a57e483aa71277c691663ebc3003663ce750708
alamscode <ahmed@eurustechnologies.com>
2020-02-27 00:32:07 +0500

    added commit check job

--- .circleci/commit_check.sh
+++ .circleci/commit_check.sh
@@ -4,7 +4,7 @@ set -e
 LATEST_COMMIT=$(git rev-parse HEAD)
 
 # latest commit where path/to/folder1 was changed
-FOLDER1_COMMIT=$(git log -1 --format=format:%H --full-diff ../Docker/SimpleApp/requirements.txt)
+FOLDER1_COMMIT=$(git log -1 --format=format:%H --full-diff Docker/SimpleApp)
 
 # latest commit where path/to/folder2 was changed
 # FOLDER2_COMMIT=$(git log -1 --format=format:%H --full-diff path/to/folder2)
