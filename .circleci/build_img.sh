b22fe717682748f02fae85c89858b06751997db9
alamscode <ahmed@eurustechnologies.com>
2020-02-27 04:31:24 +0500

    changes

--- .circleci/build_img.sh
+++ .circleci/build_img.sh
@@ -3,7 +3,7 @@ set -x
 echo "Building Image -ECR Login"
 $(aws ecr get-login --no-include-email --region us-east-1)
 echo "Building Image -Building"
-docker build -t aws-cli Docker/SimpleApp/
+docker build -t simpleapp Docker/SimpleApp/
 echo "Building Image -Tagging"
-docker tag aws-cli:latest 020046395185.dkr.ecr.us-east-1.amazonaws.com/aws-cli:$LAST_COMPLETED_BUILD_SHA
-# docker push 020046395185.dkr.ecr.us-east-1.amazonaws.com/aws-cli:latest
+docker tag simpleapp:latest 020046395185.dkr.ecr.us-east-1.amazonaws.com/simpleapp:$CIRCLE_SHA1
+docker push 020046395185.dkr.ecr.us-east-1.amazonaws.com/simpleapp:$CIRCLE_SHA1
