0b147879acd54bdba28a7ea826694b6e704e4d58
alamscode <ahmed@eurustechnologies.com>
2020-02-27 02:57:27 +0500

    changes

--- .circleci/commit_check.sh
+++ .circleci/commit_check.sh
@@ -108,7 +108,7 @@ echo -e "  $DATA"
 URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
 echo $URL
 echo ${CIRCLE_TOKEN}
-HTTP_RESPONSE=$(curl -s -u ${CIRCLE_TOKEN}: -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" $URL)
+HTTP_RESPONSE=$(curl -s -u a5baf3682207708879c5a2a0e6504e73ed5a15f0: -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" $URL)
 
 if [ "$HTTP_RESPONSE" -ge "200" ] && [ "$HTTP_RESPONSE" -lt "300" ]; then
     echo "API call succeeded."
