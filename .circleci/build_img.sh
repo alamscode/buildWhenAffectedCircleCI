a7ba453227a25f61121c5794c96ed62bb8deb80c
alamscode <ahmed@eurustechnologies.com>
2020-02-27 04:54:12 +0500

    changes

--- .circleci/build_img.sh
+++ .circleci/build_img.sh
@@ -5,6 +5,7 @@ $(aws ecr get-login --no-include-email --region us-east-1)
 echo "Building Image -Building"
 docker build -t simpleapp Docker/SimpleApp/
 echo "Building Image -Tagging"
+echo ${CIRCLE_API_USER_TOKEN}
 SHORT_GIT_HASH = $(echo $CIRCLE_SHA1 | cut -c -7)
-docker tag simpleapp:latest 020046395185.dkr.ecr.us-east-1.amazonaws.com/simpleapp:${SHORT_GIT_HASH}
-docker push 020046395185.dkr.ecr.us-east-1.amazonaws.com/simpleapp:${SHORT_GIT_HASH}
+docker tag simpleapp:latest 020046395185.dkr.ecr.us-east-1.amazonaws.com/simpleapp:"$SHORT_GIT_HASH"
+docker push 020046395185.dkr.ecr.us-east-1.amazonaws.com/simpleapp:"$SHORT_GIT_HASH"
