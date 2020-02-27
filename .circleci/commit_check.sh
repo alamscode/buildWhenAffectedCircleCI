41c7e4064118f463548e95b76cef422e200416ba
Wajahat <wajahat@nclouds.com>
2020-02-27 23:32:06 +0500

    new changes

--- .circleci/commit_check.sh
+++ .circleci/commit_check.sh
@@ -65,7 +65,8 @@ do
         COUNT=$((COUNT + 1))
         echo -e "\e[36m  [+] ${PACKAGE} \e[21m (changed in [${LATEST_COMMIT_SINCE_LAST_BUILD:0:7}])\e[0m"
     else
-        echo -e "\e[90m  [-] $PACKAGE \e[0m"
+        # echo -e "\e[90m  [-] $PACKAGE \e[0m"
+        echo -e "\e[36m  [+] ${PACKAGE} \e[21m (changed in [${LATEST_COMMIT_SINCE_LAST_BUILD:0:7}])\e[0m"
     fi
   fi
 done
