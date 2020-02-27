9dc960906d7412b498ca5db65d04ad5f04181022
Wajahat <wajahat@nclouds.com>
2020-02-28 01:17:59 +0500

    new changes

--- .circleci/commit_check.sh
+++ .circleci/commit_check.sh
@@ -60,7 +60,7 @@ do
   if [[ -z "$LATEST_COMMIT_SINCE_LAST_BUILD" ]]; then
     echo -e "\e[90m  [-] $PACKAGE \e[0m"
     if [[ $PACKAGE == "requirements.txt" ]]; then
-      echo "${LATEST_COMMIT_SINCE_LAST_BUILD:0:7}" > .circleci/checksum
+      echo "commit-${LATEST_COMMIT_SINCE_LAST_BUILD:0:7}" > .circleci/checksum
     fi
 
   else
